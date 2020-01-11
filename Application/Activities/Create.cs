2aa1e58501d213cf385f07ed294e6442cc3a6dc9
Catalina <catalina@iMac-Pro.local>
2020-01-11 08:10:59 +0700

    End of Video 194

--- Application/Activities/Create.cs
+++ Application/Activities/Create.cs
@@ -6,6 +6,9 @@ using Domain;
 using MediatR;
 using Persistence;
 using FluentValidation;
+using Application.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
 namespace Application.Activities
 {
     public class Create
@@ -36,8 +39,10 @@ namespace Application.Activities
         public class Handler : IRequestHandler<Command>
         {
             private readonly DataContext _context;
-            public Handler(DataContext context)
+            private readonly IUserAccessor _userAccessor;
+            public Handler(DataContext context, IUserAccessor userAccessor)
             {
+                _userAccessor = userAccessor;
                 _context = context;
             }
 
@@ -53,7 +58,23 @@ namespace Application.Activities
                     City = request.City,
                     Venue = request.Venue
                 };
+
                 _context.Activities.Add(activity);
+
+                var user = await _context.Users.SingleOrDefaultAsync(x =>
+                 x.UserName == _userAccessor.GetCurrentUsername());
+
+                var attendee = new UserActivity
+                {
+                    AppUser = user,
+                    Activity = activity,
+                    IsHost = true,
+                    DateJoined = DateTime.Now
+                };
+
+                _context.UserActivities.Add(attendee);
+
+
                 var success = await _context.SaveChangesAsync() > 0;
 
                 if (success) return Unit.Value;
